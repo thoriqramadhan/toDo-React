826ad3653a421f59bba527e859fc83913ab83c4f
Thoriq R <respect.thoriq@gmail.com>
2024-03-12 13:11:25 +0500

    adding tab interactivity , filtering on body

--- src/components/BodyTab.jsx
+++ src/components/BodyTab.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
-function BodyTab() {
+function BodyTab({setBodyView}) {
+  const [isClickedFinished , setIsClickedFinished] = useState(true)
+  const [isClickedGoing , setIsClickedGoing] = useState(false)
+
+  function FinishedHandler(){
+    setBodyView(true)
+    setIsClickedFinished(false)
+    setIsClickedGoing(true)
+  }
+  function OnGoingHandler(){
+    setBodyView(false)
+    setIsClickedGoing(false)
+    setIsClickedFinished(true)
+  }
   return (
     <div className='w-full h-[46px] bg-black text-white text-[24px] flex justify-between items-center px-[40px] sticky top-0'>
-        <h1>On Going</h1>
-        <h1 className='opacity-60'>Finished</h1>
+        <h1 className={`cursor-pointer transition-all delay-100 ${isClickedGoing ? 'opacity-80' : ''}`} onClick={OnGoingHandler}>On Going</h1>
+        <h1 className={`cursor-pointer transition-all delay-100 ${isClickedFinished ? 'opacity-80' : ''}`} onClick={FinishedHandler}>Finished</h1>
     </div>
   )
 }
