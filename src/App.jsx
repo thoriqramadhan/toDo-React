1a398edd5360c4dad9c2b3818b356eeb0ce202b8
Thoriq R <respect.thoriq@gmail.com>
2024-03-12 10:24:45 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='text-2xl font-bold text-yellow-400'>Hello World!</h1>
+    <h1 className='text-2xl font-bold text-yellow-400'>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
