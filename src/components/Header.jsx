67c526194bdbefade6522a21d00ce4b8df8f7d1e
Thoriq R <respect.thoriq@gmail.com>
2024-03-13 13:11:16 +0500

    Make year follow callendar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 function Header({setIsOpen}) {
+  const currentDate = new Date();
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  // Daftar nama bulan dalam setahun
+  const monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  // Mendapatkan hari dan bulan dalam format yang diinginkan
+  const dayOfWeek = daysOfWeek[currentDate.getDay()];
+  const month = monthsOfYear[currentDate.getMonth()];
+  const year = currentDate.getFullYear().toString().slice(-2);
   return (
     <>
-    <p className='text-[20px] pt-[40px]'>Saturday, July 24</p>
+    <p className='text-[20px] pt-[40px]'>{dayOfWeek}, {month} {year}</p>
     <div className="flex w-[100%] justify-between">
         <h1 className='text-[32px] font-bold'>To Do List</h1>
         <button className='w-[46px] h-[44px] bg-slate-200 rounded-full' onClick={() => setIsOpen(true)}>+</button>
