028fad7a9ca9d97501b77fd0ee4971ba835b35c5
Thoriq R <respect.thoriq@gmail.com>
2024-03-13 11:04:01 +0500

    if no data on tasks show No Data message

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,19 +5,23 @@ import Modal from './Modal'
 
 function Body({tasks , setTasks}) {
   const [bodyView , setBodyView] = useState(false)
-
-  
+  let condition;
+  if(tasks.length == 0){
+    condition = false
+  }else{
+    condition = true
+  }
   return (
     <>
     <div className='w-[100%] h-[450px] mt-[34px] overflow-scroll bg-[#D9D9D9] static'>
         <BodyTab setBodyView={setBodyView}/>
         <div className="px-[15px] mt-[20px] flex flex-col gap-y-2">
           {
-            tasks ?
+            condition ?
             bodyView ? 
             tasks.filter(task => task.isFinished).map(task => <BodyCard name={task.name} deadline={task.deadline} key={task.name} isFinished={task.isFinished} setTasks={setTasks} tasks={tasks}/>) : 
             tasks.filter(task => !task.isFinished).map(task => <BodyCard name={task.name} key={task.name} deadline={task.deadline} isFinished={task.isFinished} setTasks={setTasks} tasks={tasks}/>) :
-            <h1>No data</h1>
+            <h1 className='text-4xl font-bold text-center mt-[35%]'>No Data</h1>
           }
         </div>
     </div>
