9a661c7e333fcd4a4efd1ab0f7690e6d830582ff
Thoriq R <respect.thoriq@gmail.com>
2024-03-13 12:57:18 +0500

    making filter feature

--- src/components/ModalBody.jsx
+++ src/components/ModalBody.jsx
@@ -1,24 +1,39 @@
 import React from 'react'
+import { useState } from 'react'
 
 function ModalBody({setIsOpen ,tasks}) {
+  const [isDuplicate , setIsDuplicate] = useState(false)
     function formatData(e){
+        e.preventDefault()
         const form = e.target
         const formData = {
             name : form.elements.toDo.value,
             deadline : form.elements.deadline.value,
             isFinished: false
         }
-        tasks.push(formData)
-        setIsOpen(false)
-        e.preventDefault()
+        let isEmpty = true
+        let sameName = tasks.filter(task => task.name == formData.name) || ''
+
+        if(tasks.length >= 1){
+          isEmpty = false
+        }
+        if(sameName.length > 0 && sameName[0].name === formData.name && !isEmpty){
+          setIsDuplicate(true)
+          return
+        }
+          tasks.push(formData)
+          setIsDuplicate(false)
+          setIsOpen(false)
+
     }
   return (
     <form action="" onSubmit={formatData} className='w-full mt-[50px] mb-[30px] flex flex-col items-start justify-center px-[20px]'>
         <div className="w-full">
           <label htmlFor="" className='font-bold text-3xl'>Target</label>
           <input type="text" name="toDo" id="toDo" className='block ring-1 px-[8px] py-[5px] w-full rounded-lg mt-2' maxLength={20} required/>
+          {isDuplicate ? <span className='text-sm text-red-500'>Name is already defined.</span> : ''}
         </div>
-        <div className="w-full mt-5">
+        <div className="w-full mt-4">
           <label htmlFor="" className='font-bold text-3xl'>Deadline</label>
           <input type="date" name="deadline" id="deadline" className='block ring-1 px-[8px] py-[5px] w-full rounded-lg mt-2' required/>
         </div>
